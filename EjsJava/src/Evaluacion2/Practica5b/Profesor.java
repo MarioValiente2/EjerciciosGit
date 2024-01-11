1fe206e55f2c92818f689d62dadd59a129d8c323
“MarioValiente2” <mario.valiente2@educa.madrid.org>
2024-01-11 14:31:09 +0100

    Final

---
+++ EjsJava/src/Evaluacion2/Practica5b/Profesor.java
@@ -0,0 +1,16 @@
+package Evaluacion2.Practica5b;
+
+public class Profesor {
+
+    public void ponerNotas(Alumno notas) {
+        notas.getAsig1().setCalificacion(Math.random() * 10);
+        notas.getAsig2().setCalificacion(Math.random() * 10);
+        notas.getAsig3().setCalificacion(Math.random() * 10);
+    }
+    public double calcularMedia(Alumno notas){
+        double nota1=notas.getAsig1().getCalificacion();
+        double nota2=notas.getAsig2().getCalificacion();
+        double nota3=notas.getAsig3().getCalificacion();
+        return  ((nota1+nota2+nota3)/3);
+    }
+}
